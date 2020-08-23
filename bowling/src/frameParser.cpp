edc6cc6e6ddd0066fe9e3fd1c6c4973f4827f4f7
kuchichan <pawel.kucharski@hotmail.com>
2020-08-23 22:36:10 +0200

    algo + rename

--- bowling/src/frameParser.cpp
+++ bowling/src/frameParser.cpp
@@ -2,27 +2,28 @@
 
 #include <iostream>
 #include <iterator>
+#include <algorithm>
 
 parsedFrame FrameParser::parse(const std::string& gamePoints) {
   std::vector<size_t> parsed {};
-  size_t last_parsed{};
+  size_t last_parsed_char{};
   auto tokenized = Tokenizer::splitToFramesAlt(gamePoints);
   std::transform(tokenized.second.cbegin(),
 		 tokenized.second.cend(),
 		 back_inserter(parsed),
-		 [&last_parsed] (char el) {
+		 [&last_parsed_char] (char el) {
 		   if (el == 'X') {
-		     last_parsed = 10;
-		     return last_parsed;
+		     last_parsed_char = 10;
+		     return last_parsed_char;
 		   } else if (el == '/') {
-		     last_parsed = 10 - last_parsed;
-		     return last_parsed;
+		     last_parsed_char = 10 - last_parsed_char;
+		     return last_parsed_char;
 		   } else if (el == '-') {
-		     last_parsed = 0;
-		     return last_parsed;
+		     last_parsed_char = 0;
+		     return last_parsed_char;
 		   } else {
-		     last_parsed = el - '0';
-		     return last_parsed;
+		     last_parsed_char = el - '0';
+		     return last_parsed_char;
 		   }
 		 });
   
@@ -35,6 +36,7 @@ tokenizedFrame Tokenizer::splitToFramesAlt(const std::string& gamePoints) {
     std::string name = "";
     
     next = gamePoints.find(":", prev);
+    
     if (next != std::string::npos) {
       name = gamePoints.substr(prev, next - prev);
       prev = next + 1;
